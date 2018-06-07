73cc08bccedd1807640cbf8c56722e01e03c796f
Giuseppe <giuseppe.p.carlone@gmail.com>
2018-06-07 11:24:40 +0200

    Upload

---
+++ src/Cliente.java
@@ -0,0 +1,152 @@
+
+/**
+ * La classe Cliente estende la classe Persona rappresentando un cliente astratto di una biblioteca.
+ * Presenta metodi sui noleggi all'attivo del cliente, la restituzione e codice fiscale.
+ * 
+ * @author Carlone Giuseppe Pio
+ * @author Mambella Alessandra
+ * @author Sichetti Dylan
+ *
+ */
+public class Cliente extends Persona {
+    
+    
+    /**
+     * Numero di noleggi attivi
+     */
+    
+    private int prestiti;    
+    
+    /**
+     * Codice fiscale del cliente
+     */
+    
+    private String codicef;
+    
+                        /** Costruttori */	
+	
+    /** Costruttore di default: definisce gli attributi per il cliente come il numero dei prestiti attivi e il codice fiscale.
+     *  Inoltre eredita gli attributi della classe Persona.
+     */
+    
+    public Cliente()
+    {
+        super();
+        prestiti = 0;
+	codicef = "CodicefD";
+    }
+    
+    /**
+     * Istanzia l'oggetto della classe cliente con i parametri passati
+     * @param NomeC Nome del cliente
+     * @param CognomeC Cognome del cliente
+     * @param CodicefC Codice fiscale del cliente
+     */
+    
+    public Cliente(String NomeC,String CognomeC,String CodicefC)
+        {
+            this.nome = NomeC;  // Se l'attributo fosse 'private', avremmo errore.
+            this.cognome = CognomeC;
+            prestiti = 0;
+            codicef = CodicefC;
+        }
+    
+                        /** Metodi Get */
+    
+    /**
+     * Restituisce il numero di noleggi attivi del cliente
+     * @return numero di noleggi
+     */
+    
+    public int getPrestiti()
+        {
+            return prestiti;
+        }
+    
+    /**
+     * Restituisce una stringa contenente il codice fiscale del cliente
+     * @return il codice fiscale
+     */
+    
+    public String getCodiceFisc()
+        {
+            return codicef;
+        }
+    
+                        /** Metodi Set */
+    
+    /**
+     * Imposta il numero dei noleggi del cliente
+     * @param PrestitiC
+     */
+    
+    public void setPrestiti(int PrestitiC)
+        {
+            prestiti = PrestitiC;
+        }
+    
+    /**
+     * Imposta il codice fiscale del cliente
+     * @param CodicefC codice fiscale da impostare
+     */
+    
+    public void setCodiceFisc(String CodicefC)
+        {
+            codicef = CodicefC;
+        }
+    
+      
+    /**
+     * Aumenta di 1 il numero di noleggi del cliente
+     */
+    
+    public void Nolegg()
+        {
+           prestiti++;
+        }
+    
+    /**
+     * Decresce di 1 il numero di noleggi all'attivo del cliente se questo è maggiore di 0
+     */
+    
+    public void Restituzione()
+        {
+            if(prestiti > 0)
+                {
+                    prestiti--;
+                }
+
+        }
+    
+    /**
+     * Controlla se il cliente è uguale al secondo (passato come parametro). 
+     * Il metodo confronta il nome, il cognome e il codice fiscale
+     * @param c cliente con cui operare il confronto
+     * @return una variabile booleana che rappresenta lo stato di uguaglianza o meno tra due clienti 
+     */
+    
+    public boolean equals(Cliente c)
+        {
+            if(this.nome.equalsIgnoreCase(c.getNome()) && this.cognome.equalsIgnoreCase(c.getCognome()) &&
+                    codicef.equalsIgnoreCase(c.getCodiceFisc()))
+
+                {
+                    return true;
+                }
+
+            else 
+                {
+                    return false;
+                }
+        }
+    
+    /**
+     * Converte in stringa l'oggetto cliente
+     * @return la stringa rappresentante il cliente nel formato: nome cognome codice fiscale
+     */
+    public String toString()
+        {
+            return super.toString()+" "+codicef;
+        }
+    
+}
---
+++ src/Data.java
@@ -0,0 +1,332 @@
+import java.lang.Comparable;
+
+/**
+ * La classe Data descrive giorno,mese ed anno di una data
+ * controllando che i valori inseriti rispettino le bisestilità e le lunghezze effettive dei mesi.
+ * 
+ * @author Carlone Giuseppe Pio
+ * @author Mambella Alessandra
+ * @author Sichetti Dylan
+ 
+ * L'utilizzo dell'interfaccia Comparable permette un confronto in 
+ * ordine di precedenza tra due istanze di tipo Data
+ */
+
+public class Data implements Comparable{
+    
+       
+    	/**
+	 * Il giorno della data
+	 */
+        
+        private int giorno;
+	
+	/**
+	 * Il mese della data
+	 */
+        
+        private int mese;
+	
+	/**
+	 * L'anno della data
+	 */
+        
+        private int anno;
+	
+	/**
+	 * La possibile bisestilità di un anno
+	 */
+        
+        private boolean bis;
+	private static final int Min = 1;            // private static final associa ad una
+	private static final int MaxGiorni = 31;     // variabile una costante
+	private static final int MaxMesi = 12;
+	private static final int MinAnno = 2000;
+	private static final int[] Special = { 4 , 100 , 400 };
+     
+    
+        /**
+	 * Il costruttore di default genera una data nel formato 
+	 * (gg/mm/aaaa) impostando i valori al giorno 01/01/2000
+	 */
+	
+	public Data()
+            {
+		giorno = Min;
+		mese = Min;
+		anno = MinAnno;
+            }   
+	
+	/**
+	 * Attravero questo costruttore, passando i parametri giorno, mese, anno 
+	 * viene inizializzato un oggetto di tipo data specifico, il quale
+	 * risulterà avere le caratteristiche scelte dall'utente (a meno che non
+         * sia una data non esistente, in quel caso verrà corretto);
+	 * @param g il giorno che verrà impostato
+	 * @param m il mese che verrà impostato
+	 * @param a l'anno che verrà impostato
+	 * 
+	 */
+	
+	public Data(int g, int m, int a)
+            {
+		setAnno(a);
+		setMese(m);
+		setGiorno(g);
+            }
+	
+	/**
+	 * Restituisce il giorno
+	 * @return Il giorno
+	 */
+	
+	public int getGiorno()
+            {
+		return giorno;
+            }
+	
+	/**
+	 * Restituisce il mese
+	 * @return il mese 
+	 */
+	
+	public int getMese()
+            {
+		return mese;
+            }
+	
+	/**
+	 * Restituisce l'anno
+	 * @return l'anno 
+	 */
+	
+	public int getAnno()
+            {
+                return anno;
+            }
+	
+	/**
+	 * Permette di impostare il valore del giorno di una data
+	 * @param g il giorno da impostare
+	 */
+	
+	public void setGiorno(int g)
+            {
+		int GiorniTot = ControlloGiorniMese();
+		if( g > GiorniTot)
+                    {
+			g = GiorniTot;
+                    }
+		
+		if(g < Min)
+                    {
+			g = Min;
+                    }
+		
+		giorno = g;
+            }
+	
+	/**
+	 * Permette di impostare il valore del mese di una data
+	 * @param m il mese da impostare
+	 */
+	
+	public void setMese(int m)
+            {
+		if( m > MaxMesi)
+                    {
+			m = MaxMesi;
+                    }
+		
+		else if(m < Min)
+                    {
+			m = Min;
+                    }
+		
+		mese = m;
+            }
+	
+	/**
+	 * Permette di impostare il valore dell'anno di una data e calcola
+         * se questo è bisestile o no
+	 * @param a l'anno da impostaare
+	 */
+	
+	public void setAnno(int a)
+            {
+		if( a < 0)
+                    {
+			anno = 0;
+			bis = false;
+                    }
+		
+		else
+                    {
+			anno = a;
+			if(anno%Special[0]==0 && (a%Special[1]!=0 || a%Special[2]==0))
+                            {
+				bis = true;
+                            }
+			else
+                            {
+				bis = false;
+                            }
+                    }
+            }
+	
+	
+	private int ControlloGiorniMese()
+	{
+		int GiorniTot = MaxGiorni;
+                
+		switch(this.mese)
+		{
+			case 4 : 
+			case 6 :
+			case 9 :
+			case 11: GiorniTot = 30; break;
+				
+			case 2 :
+			if(bis)
+			{
+				GiorniTot=29;
+			}
+			else
+			{
+				GiorniTot=28;
+			}
+			break;
+		}
+		
+		return GiorniTot;
+	}
+	
+	/**
+	 * Inserito un numero intero di giorni calcola la data esatta dopo i giorni immessi
+	 * @param g numero di giorni da aggiungere alla data attuale
+	 * @return la data esatta che ci sarà dopo quel quantitativo di giorni
+	 */
+	
+	public Data RiordGior(int g)
+	{
+		int GiorniTot=ControlloGiorniMese();
+		int giorni=giorno;
+		int mesi=mese;
+		int anni=anno;
+		boolean bisestile=bis;
+		giorno=giorno+g;
+		while(giorno>GiorniTot)
+		{
+			giorno=giorno-GiorniTot;
+                        mese++;
+			if(mese>MaxMesi)
+			{
+				mese=mese-MaxMesi;
+				anno++;
+				if(anno%Special[0]==0&&(anno%Special[1]!=0||anno%Special[2]==0))
+				{
+					bis=true;
+				}
+				else
+				{
+					bis=false;
+				}
+			}
+			GiorniTot=ControlloGiorniMese();
+		}
+		
+		Data data=new Data(giorno,mese,anno);
+                giorno=giorni;
+                mese=mesi;
+                anno=anni;
+                bis=bisestile;
+                return data;  
+	}
+	
+	/**
+	 * Converte una composizione di attributi giorno, mese, anno in una 
+	 * stringa contenente tali parametri nel formato gg/mm/aaaa
+	 * @return la data richiesta nel formato gg/mm/aaaa
+	 */
+	
+	public String toString()
+        {
+                String s=giorno+"/"+mese+"/"+anno;    
+                return s;
+        }
+	
+	/**
+	 * Applica un confronto tra date e controlla se sono uguali confrontando giorno, mese e anno
+	 * @param data rappresenta la data da confrontare
+	 * @return una variabile booleana indicante l'uguaglianza o non tra due date
+	 */
+	
+	public boolean equals(Data data)
+        {
+                if(data.getGiorno()==giorno && data.getMese()==mese && data.getAnno()==anno)
+		{
+                        return true;
+		}
+                else 
+		{
+			return false;
+		}
+        }
+	
+	/**
+	 * Implementando questo metodo viene reso possibile il riordinamento 
+	 * a sfondo temporale tra due date
+	 * 
+	 * @param other rappresenta l'altra data da inserire nel confronto temporale
+	 * @return un intero a seconda dei casi che simboleggia la posizione di una data rispetto ad un'altra
+	 */
+	
+        public int compareTo(Object other)
+        {
+            Data data2=(Data)other;
+            if(anno>data2.getAnno() || anno<data2.getAnno())
+            {
+                if(anno>data2.getAnno())
+		{
+                    return 1;
+		}
+                else 
+		{
+			return -1;
+		}
+            }
+            else
+            {
+                if(mese>data2.getMese() || mese<data2.getMese())
+                {
+                    if(mese>data2.getMese())
+		    {
+			    return 1;
+		    }
+                    else 
+		    {
+			    return -1;
+		    }
+                }
+                else
+                {
+                    if(giorno>data2.getGiorno() || giorno<data2.getGiorno())
+                    {
+                        if(giorno>data2.getGiorno())
+			{
+				return 1;
+			}
+                        else 
+			{
+				return -1;
+			}
+                    }
+                    else
+		    {
+			    return 0;
+		    }
+                }
+            }
+        }               
+            
+}
